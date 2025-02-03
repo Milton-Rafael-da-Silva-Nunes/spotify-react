fb723288d83ce3b66ecaede2f06b42a7b249fc35
rafael_nunes <rafa.nuns17@gmail.com>
2025-02-03 18:07:56 -0300

    Adicionando Biblioteca no Menu-header

--- src/componentes/Menu/Menu.js
+++ src/componentes/Menu/Menu.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faSearch, faBook, faPlus } from '@fortawesome/free-solid-svg-icons';
 import './Menu.css';
 
 import logoPrincipal from '../../assets/icons/logo-spotify.png';
@@ -12,20 +12,38 @@ const Menu = () => {
                 <img src={logoPrincipal} alt="Logo Principal" />
             </div>
 
-            <ul>
+            <ul className="menu__links">
                 <li>
-                    <a href="#">
+                    <a href="/" aria-label="Início">
                         <FontAwesomeIcon icon={faHouse} />
                         <span> Início</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="/" aria-label="Buscar">
                         <FontAwesomeIcon icon={faSearch} />
                         <span> Buscar</span>
                     </a>
                 </li>
             </ul>
+
+
+            <div className="container__biblioteca">
+                <div className="titulo__biblioteca">
+                    <a href="/" aria-label="Sua Biblioteca">
+                        <FontAwesomeIcon icon={faBook} />
+                        <span>Sua Biblioteca</span>
+                    </a>
+                    <FontAwesomeIcon icon={faPlus} style={{fontSize: '24px'}}/>
+                </div>
+
+                <div className="container__playlist">
+                    <h1>Crie sua primeira Playlist</h1>
+                    <p>É fácil, vamos te ajudar</p>
+                </div>
+
+            </div>
+
         </nav>
     );
 }
