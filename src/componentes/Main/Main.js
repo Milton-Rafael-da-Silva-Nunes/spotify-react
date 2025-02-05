7a689d6497bcea0a091b2611b3a7fd1721ad9d64
rafael_nunes <rafa.nuns17@gmail.com>
2025-02-05 12:17:03 -0300

    Adicionando novas variáveis de estilo e refatorando a estrutura do componente Main

--- src/componentes/Main/Main.js
+++ src/componentes/Main/Main.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+import "./Main.css";
 
 // Importando as imagens manualmente
 import img1 from "../../assets/playlist/1.jpeg";
@@ -37,19 +40,53 @@ const playlists = [
 
 const Main = () => {
     return (
-        <main>
-            <div className="main-scroll__container">
-                <div className="main-scroll__conteudo">
-                    <section className="main-scroll__section">
-                        {playlists.map((playlist, index) => (
-                            <a href="/" className="cards" key={index}>
-                                <div className={`cards card-${index + 1}`}>
-                                    <img src={playlist.img} alt={`capa-${index + 1}`} />
-                                    <span>{playlist.title}</span>
+        <main className="main-container">
+            <div className="main-container__header">
+                {/* Seção de boas-vindas */}
+                <div className="main-header__playlists">
+                    <div className="playlists">
+                        <h1>Boas vindas</h1>
+                        <h2>Navegar por todas as seções</h2>
+                    </div>
+
+                    {/* Scrollable container para as playlists */}
+                    <div className="main-scroll__container">
+                        <div className="main-scroll__conteudo">
+                            <section className="main-scroll__section">
+                                {playlists.map((playlist, index) => (
+                                    <a href="/" className="cards" key={index}>
+                                        <div className={`cards card-${index + 1}`}>
+                                            <img src={playlist.img} alt={`capa-${index + 1}`} />
+                                            <span>{playlist.title}</span>
+                                        </div>
+                                    </a>
+                                ))}
+                            </section>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Seção de artista (escondida por padrão) */}
+                <div id="resultado-artista" className="hidden">
+                    <div className="grid-container">
+                        <div className="artista-card">
+                            <div className="card-img">
+                                <img id="artista-img" className="artista-img" alt="Imagem Artista" />
+                                <div className="play">
+                                    <span>
+                                        <FontAwesomeIcon icon={faPlay} />
+                                    </span>
                                 </div>
-                            </a>
-                        ))}
-                    </section>
+                            </div>
+
+                            <div className="card-texto">
+                                <a title="Foo Fighters" className="vst" href="/">
+                                    <span className="artista-nome">Foo Fighters</span>
+                                    <span className="artista-categoria">Artista</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
